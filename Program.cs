c20f4cb39313111de987e4244e7957927c209591
przemek milke <theblimek@gmail.com>
2024-03-09 10:47:40 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNet2_7_2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj liczbę: ");
+            int liczba = Int32.Parse(Console.ReadLine());
+
+            if (liczba % 2 == 0) 
+            {
+
+                Console.WriteLine("Liczba jest liczbą parzystą");
+            }
+
+            else
+
+            {
+                Console.WriteLine("Liczba nie jest lisczbą parzystą");
+
+            }
+            Console.ReadLine();
+
+        }
+  
+    }
+}
